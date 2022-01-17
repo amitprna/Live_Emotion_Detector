dfb5e3b1a40921a2d073ff568996d9de112b8200
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2022-01-17 18:41:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import cv2
+import streamlit as st
+
+st.title('Emotion Detection')
+
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+cam = cv2.VideoCapture(0)
+
+while run:
+  ret, frame = cam.read()
+  FRAME_WINDOW.image(frame)
+else:
+  st.write('stopped)
