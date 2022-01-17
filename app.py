a2b832bffd43d4897c3120ca91f64c1e371f1866
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2022-01-17 20:35:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,49 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, \
-    RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
 import av
 import threading
 
-RTC_CONFIGURATION = \
-    RTCConfiguration({'iceServers': [{'urls': ['stun:stun.l.google.com:19302'
-                     ]}]})
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
-st.set_page_config(page_title='Emotion Detection',
-                   page_icon='\xf0\x9f\xa4\x96')
-st.header('Scanning live feed.....')
+st.set_page_config(page_title="Emotion Detector", page_icon="🤖")
+st.title('Processing live feed.......')
 
+if task_name == task_list[0]:
+    style_list = ['color', 'black and white']
 
-class VideoProcessor(VideoProcessorBase):
+    st.sidebar.header('Style Selection')
+    style_selection = st.sidebar.selectbox("Choose your style:", style_list)
 
-    def __init__(self):
-        self.model_lock = threading.Lock()
-        self.style = style_list[0]
+    class VideoProcessor(VideoProcessorBase):
+        def __init__(self):
+            self.model_lock = threading.Lock()
+            self.style = style_list[0]
 
-    def update_style(self, new_style):
-        if self.style != new_style:
-            with self.model_lock:
-                self.style = new_style
+        def update_style(self, new_style):
+            if self.style != new_style:
+                with self.model_lock:
+                    self.style = new_style
 
-    def recv(self, frame):
+        def recv(self, frame):
+            # img = frame.to_ndarray(format="bgr24")
+            img = frame.to_image()
+            if self.style == style_list[1]:
+                img = img.convert("L")
 
-        # img = frame.to_ndarray(format="bgr24")
+            # return av.VideoFrame.from_ndarray(img, format="bgr24")
+            return av.VideoFrame.from_image(img)
 
-        img = frame.to_image()
-        if self.style == style_list[1]:
-            img = img.convert('L')
-
-        # return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-        return av.VideoFrame.from_image(img)
-
-    ctx = webrtc_streamer(key='example',
-                          video_processor_factory=VideoProcessor,
-                          rtc_configuration=RTC_CONFIGURATION,
-                          media_stream_constraints={'video': True,
-                          'audio': False})
+    ctx = webrtc_streamer(
+        key="example",
+        video_processor_factory=VideoProcessor,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={
+            "video": True,
+            "audio": False
+        }
+    )
 
     if ctx.video_processor:
         ctx.video_transformer.update_style(style_selection)
