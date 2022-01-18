be88dffaef17a1a7ca40471bd505ea23a6037177
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2022-01-18 12:26:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 from deepface import DeepFace
 import streamlit as st
 from streamlit_webrtc import  VideoProcessorBase, webrtc_streamer,RTCConfiguration
+import av
 
 #inp_image = st.camera_input('say cheese.......')
 RTC_CONFIGURATION = RTCConfiguration(
